793e23cd9b4251203b225c32f1191e3c7cb99f71
Eduardo Pacheco <jpachecom1703@alumno.ipn.mx>
2024-07-16 18:37:38 -0600

    First script

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 18:17:32 2024
+
+@author: MARIBEL
+"""
+print("Hello world!")
+
+print('Brasil','tiene', 5, 'titutlos' ,'mundiales' ,'de futbol.', sep='-')
+
+print('Brasil','tiene', 5, 'titutlos' ,'mundiales' ,'de futbol.', sep=' ', end='')
+
+pais = "Argentina"
+titulos = 4
+
+print(pais, "tiene",  titulos, "mundiales de futbol.")
