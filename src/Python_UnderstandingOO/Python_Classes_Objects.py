ae0c644a9568a4e80b02c397da6496b4ad45e694
Eduardo Pacheco <jpachecom1703@alumno.ipn.mx>
2024-08-15 20:54:08 -0600

    Methods-Implementation

--- src/Python_UnderstandingOO/Python_Classes_Objects.py
+++ src/Python_UnderstandingOO/Python_Classes_Objects.py
@@ -10,11 +10,24 @@ class Account:
     
     #Constructor
     def __init__(self, number, holder, balance, agency, limit):
-        print("Creando una cuenta bancaria...")
+        print("Creating bank account...")
         self.number = number
         self.holder = holder
         self.balance = balance
         self.agency = agency
         self.limit = limit
+    
+    def withdraw(self, value):
+        self.balance -= value
+        
+    def deposit(self, value):
+        self.balance += value
+
+    def extract(self):
+        print(f'The balance of the account of {self.holder} is: {self.balance}')
         
 account_1 = Account(123, 'Eduardo', 55.0, '001', 1000.0)
+
+account_1.deposit(700)
+account_1.withdraw(40)
+account_1.extract()
