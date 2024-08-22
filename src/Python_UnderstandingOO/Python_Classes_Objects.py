fbb9fd2bb02d8720aeba4283fb128c6c5983f652
Eduardo Pacheco <jpachecom1703@alumno.ipn.mx>
2024-08-21 19:42:43 -0600

    Properties

--- src/Python_UnderstandingOO/Python_Classes_Objects.py
+++ src/Python_UnderstandingOO/Python_Classes_Objects.py
@@ -11,25 +11,35 @@ class Account:
     #Constructor
     def __init__(self, number, holder, balance, agency, limit):
         print("Creating bank account...")
-        self._number = number #an underscore before the variable, it is declared as a private attribute
-        self._holder = holder
-        self._balance = balance
-        self._agency = agency
-        self._limit = limit
+        self.__number = number #two underscore before the variable, it is declared as a private attribute
+        self.__holder = holder
+        self.__balance = balance
+        self.__agency = agency
+        self.__limit = limit
     
     def withdraw(self, value):
-        self._balance -= value
+        self.__balance -= value
         
     def deposit(self, value):
-        self._balance += value
+        self.__balance += value
 
     def extract(self):
-        print(f'The balance of the account of {self._holder} is: {self._balance}')
+        print(f'The balance of the account of {self.__holder} is: {self.__balance}')
     
     #Encapsulation
     def transfer(self, value, destination):
         self.withdraw(value)
         destination.deposit(value)
+        
+    def get_balance(self):
+        return self.__balance
+    
+    def get_holder(self):
+        return self.__holder
+    
+    def set_limit(self, limit):
+        self.__limit = limit
+        return self.__limit
         
 account_1 = Account(123, 'Eduardo', 55.0, '001', 1000.0)
 account_2 = Account(123, 'Jesus', 100, '002', 1000.0)
@@ -56,4 +66,25 @@ account_2 = Account(123, 'Jesus', 50, '002', 1000.0)
 account_1.transfer(100.0, account_2)
 account_1.extract()
 account_2.extract()
+print("\n")
 
+#Properties
+print(f"{account_1.get_balance()}\n")
+
+class Client:
+    def __init__(self, name):
+        self.__name = name
+     
+    @property
+    def name(self):
+        return self.__name.title()
+    
+    @name.setter
+    def name(self, name):
+        self.__name = name
+    
+client = Client("eduardo")
+print(client.name)
+
+client.name = "daniela"
+print(client.name)
