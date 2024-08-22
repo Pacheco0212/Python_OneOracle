7c94ed205f416a0d520a4808b9586906ce3552ce
Eduardo Pacheco <jpachecom1703@alumno.ipn.mx>
2024-08-21 18:51:52 -0600

    Encapsulation

--- src/Python_UnderstandingOO/Python_Classes_Objects.py
+++ src/Python_UnderstandingOO/Python_Classes_Objects.py
@@ -11,23 +11,49 @@ class Account:
     #Constructor
     def __init__(self, number, holder, balance, agency, limit):
         print("Creating bank account...")
-        self.number = number
-        self.holder = holder
-        self.balance = balance
-        self.agency = agency
-        self.limit = limit
+        self._number = number #an underscore before the variable, it is declared as a private attribute
+        self._holder = holder
+        self._balance = balance
+        self._agency = agency
+        self._limit = limit
     
     def withdraw(self, value):
-        self.balance -= value
+        self._balance -= value
         
     def deposit(self, value):
-        self.balance += value
+        self._balance += value
 
     def extract(self):
-        print(f'The balance of the account of {self.holder} is: {self.balance}')
+        print(f'The balance of the account of {self._holder} is: {self._balance}')
+    
+    #Encapsulation
+    def transfer(self, value, destination):
+        self.withdraw(value)
+        destination.deposit(value)
         
 account_1 = Account(123, 'Eduardo', 55.0, '001', 1000.0)
+account_2 = Account(123, 'Jesus', 100, '002', 1000.0)
 
-account_1.deposit(700)
-account_1.withdraw(40)
+account_1._balance = 5000
 account_1.extract()
+print("\n")
+
+
+#Encapsulation
+value = 100
+
+account_1.extract()
+account_1.withdraw(value)
+account_1.extract()
+account_2.extract()
+account_2.deposit(value)
+account_2.extract()
+print("\n")
+
+account_1 = Account(123, 'Eduardo', 500.0, '001', 1000.0)
+account_2 = Account(123, 'Jesus', 50, '002', 1000.0)
+
+account_1.transfer(100.0, account_2)
+account_1.extract()
+account_2.extract()
+
